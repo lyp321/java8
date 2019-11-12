d2cc4e064ecc5b3a0925d80e5fcd83bf23290370
liyp <liyp@ebaiyihui.com>
2019-11-12 18:27:41 +0800

    java8新特性

---
+++ src/main/java/com/lyp/stream/config/Swagger2.java
@@ -0,0 +1,55 @@
+package com.lyp.stream.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * swagger配置
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+
+    //是否开启swagger，正式环境一般是需要关闭的，可根据springboot的多环境配置进行设置
+    @Value(value = "${swagger.enabled}")
+    Boolean swaggerEnabled;
+
+    //swagger2的配置文件，这里可以配置swagger2的一些基本的内容，比如扫描的包等等
+    @Bean
+    public Docket createRestApi() {
+
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .enable(swaggerEnabled)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.ebaiyihui"))
+                .paths(PathSelectors.any())
+                .build().pathMapping("/");
+    }
+
+    //构建 api文档的详细信息函数,注意这里的注解引用的是哪个
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                //页面标题
+                .title("java8新特性")
+                //描述
+                .description("java8新特性api")
+                //创建人
+                .contact(new Contact("liyp", "", ""))
+                //版本号
+                .version("1.0")
+                //描述
+                .build();
+    }
+
+}
---
+++ src/main/java/com/lyp/stream/service/StreamFilter.java
@@ -0,0 +1,9 @@
+package com.lyp.stream.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+
